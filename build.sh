2d54eb3b75b54c4a150d0fd90e73234cc3985cbf
Eyzi <contact@eyzi.dev>
2021-12-20 11:59:53 -0500

    chore: market package only

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ if [ -d "$2/old-game" ]; then
 fi
 
 echo "Building the project at $2..."
-if ../renpy/renpy.sh ../renpy/launcher distribute $2; then
+if ../renpy/renpy.sh ../renpy/launcher distribute --package market $2; then
     built_dir=$(ls | grep '\-dists')
     echo ::set-output name=dir::$built_dir
     echo ::set-output name=version::${built_dir%'-dists'}
